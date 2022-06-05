4e4ed89fd8397f6f773021aed286520ed178737e
Varich Boonsanong <varicb@attu5.cs.washington.edu>
2022-06-05 12:54:45 -0700

    1. add prettier

--- frontend/src/components/Transformer/index.tsx
+++ frontend/src/components/Transformer/index.tsx
@@ -1,6 +1,37 @@
 import tw from 'twin.macro'
 import {useState} from 'react';
 
+const UploadAndDisplayImage = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+  
+    return (
+      <div>
+        <h1>Upload and Display Image usign React Hook's</h1>
+        {selectedImage && (
+          <div>
+          <img alt="not fount" width={"250px"} src={URL.createObjectURL(selectedImage)} />
+          <br />
+          <button onClick={()=>setSelectedImage(null)}>Remove</button>
+          </div>
+        )}
+        <br />
+       
+        <br /> 
+        <input
+          type="file"
+          name="myImage"
+          onChange={(event: any) => {
+            if (!event || !event.target) {
+               console.log("There is an error while uploading file") 
+            }
+            console.log(event.target.files[0]);
+            setSelectedImage(event.target.files[0]);
+          }}
+        />
+      </div>
+    );
+  };
+
 
 const Index = () => {
 
