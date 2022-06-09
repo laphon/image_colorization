48778791d601fe83b7c026c2ce04bf44deeeaf4e
Varich Boonsanong <varicb@attu5.cs.washington.edu>
2022-06-09 00:44:41 -0700

    use g run

--- frontend/src/constant.ts
+++ frontend/src/constant.ts
@@ -1,3 +1,4 @@
-export const HOST_NAME =
-  "https://weak-flavor-classifieds-coordination.trycloudflare.com";
+export const HOST_NAME = "https://backend-hxjapihltq-uw.a.run.app";
+// export const HOST_NAME =
+//   "https://weak-flavor-classifieds-coordination.trycloudflare.com";
 export const PROXY_HOST = "https://cors-anywhere-hxjapihltq-uw.a.run.app";
