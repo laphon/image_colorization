5dd570efb1f0949e995474dc63f8c4139fa7b9b0
Varich Boonsanong <varicb@attu5.cs.washington.edu>
2022-06-08 23:23:38 -0700

    removing unsued code

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -11,7 +11,7 @@ const Title = () => (
       backgroundRepeat: "no-repeat",
       opacity: "0.9",
     }}
-    tw="flex w-full pt-24 pb-60 bg-red-200 inline mb-16"
+    tw="flex w-full pt-24 pb-60 bg-red-200 mb-16"
   >
     <h1 tw="text-center text-5xl font-semibold">Deep Image Colorizer</h1>
   </div>
