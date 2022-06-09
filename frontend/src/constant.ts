36d0294472d932a63500f88369018f6de760fd4e
Varich Boonsanong <varicb@attu5.cs.washington.edu>
2022-06-09 00:03:14 -0700

    change api

--- frontend/src/constant.ts
+++ frontend/src/constant.ts
@@ -1,2 +1,3 @@
-export const HOST_NAME = "https://backend-hxjapihltq-uw.a.run.app";
-export const PROXY_HOST = "https://cors-anywhere-hxjapihltq-uw.a.run.app"
+export const HOST_NAME =
+  "https://weak-flavor-classifieds-coordination.trycloudflare.com";
+export const PROXY_HOST = "https://cors-anywhere-hxjapihltq-uw.a.run.app";
