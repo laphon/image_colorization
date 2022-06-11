0382e3af44fdab6f78299a9f4a9373865a6c4db7
Varich Boonsanong <varicb@attu5.cs.washington.edu>
2022-06-11 07:31:08 -0700

    update cf ur

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,6 @@ if __name__ == '__main__':
     import uvicorn
 
     try:
-        uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
+        uvicorn.run('main:app', host='0.0.0.0', port=8000)
     except Exception:
         pass
