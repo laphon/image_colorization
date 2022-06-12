9b9ef9a670c7b83eff3d5aebd2c9c291dd302b84
Varich <thapecroth@gmail.com>
2022-06-12 15:14:36 -0700

    change url

--- frontend/src/constant.ts
+++ frontend/src/constant.ts
@@ -1,4 +1,4 @@
 // export const HOST_NAME = "https://backend-hxjapihltq-uw.a.run.app";
 export const HOST_NAME =
-  "https://herein-uw-reporting-discrimination.trycloudflare.com";
+  "https://gsm-wax-detective-asp.trycloudflare.com";
 export const PROXY_HOST = "https://cors-anywhere-hxjapihltq-uw.a.run.app";
